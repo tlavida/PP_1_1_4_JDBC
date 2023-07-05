fcd5ccafa7f459e8d80c014cccbce36ae410d660
tlavida <93471499+tlavida@users.noreply.github.com>
2023-07-05 22:16:05 +0300

    PP_1_1_4_JDBC
    PP_1_1_4_JDBC

---
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,25 @@
+package jm.task.core.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Util {
+    private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/first_db";
+    private static final String DB_USERNAME = "root";
+    private static final String DB_PASSWORD = "root";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName(DB_DRIVER);
+            connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+            System.out.println("Соединение выполнено");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            System.out.println("Ошибка соединения");
+        }
+        return connection;
+    }
+}
